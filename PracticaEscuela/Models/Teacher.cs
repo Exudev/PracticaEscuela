0779fb591e9a132c9cce970eaad520d2ca9d23cf
manuelusion <manuelmanonsanchez@gmail.com>
2023-01-05 21:35:10 -0400

    teacher subject changes

--- PracticaEscuela/Models/Teacher.cs
+++ PracticaEscuela/Models/Teacher.cs
@@ -10,15 +10,50 @@ namespace PracticaEscuela.Models
     {
         public Carreer Carreer { get; set; }
         public List<Subject> ImpartingSubjects { get; set; }
-    }
 
-    public void addSubject(Subject subject)
-    {
-
-
-        foreach (var item in collection)
+        public void AddSubject(Subject subsub)
         {
-
+            if (ContainsSubject(subsub) && SameDayCheck(subsub))
+            {
+                throw new Exception("Subject is already present");
+            }
+            else
+            {
+                this.ImpartingSubjects.Add(subsub);
+            }
         }
+        public void DropSubject(Subject subsub)
+        {
+            if (ContainsSubject(subsub))
+            {
+                this.ImpartingSubjects.Remove(subsub);
+            }
+            else { throw new Exception("Subject does not exist"); }
+        }
+        public bool ContainsSubject(Subject subsub)
+        {
+            return this.ImpartingSubjects.Contains(subsub);
+        }
+
+        public bool SameDayCheck(Subject subsub)
+        {
+            bool sameDay = false;
+            if(ImpartingSubjects.Count != 0)
+            {
+                foreach (Subject subject in ImpartingSubjects)
+                {
+                    if(subject.ImpartedDay == subsub.ImpartedDay)
+                    {
+                        sameDay = true;
+                    } 
+                        
+                      
+                }
+            }
+            return sameDay;
+        }
+            
     }
+
+ 
 }
