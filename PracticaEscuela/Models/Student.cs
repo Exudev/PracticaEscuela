0183628b01d877c9f5ff28e023d037869e1d70c8
Muirghen <52637101+Muirghen@users.noreply.github.com>
2023-01-05 21:17:55 -0400

    Added student's subject management

--- PracticaEscuela/Models/Student.cs
+++ PracticaEscuela/Models/Student.cs
@@ -9,7 +9,31 @@ namespace PracticaEscuela.Models
 {
     internal class Student : Person
     {
+        public Student(Carreer chosenCarreer) { this.Carreer = chosenCarreer; }
         public Carreer Carreer { get; set; }
         public List<Subject> ChosenSubjects { get; set; }
+        public void AddSubject(Subject subsub)
+        {
+            if (ContainsSubject(subsub))
+            {
+                throw new Exception("Subject is already present");
+            }
+            else
+            {
+                this.ChosenSubjects.Add(subsub);
+            }
+        }
+        public void DropSubject(Subject subsub) 
+        {
+            if (ContainsSubject(subsub))
+            {
+                this.ChosenSubjects.Remove(subsub);
+            }
+            else { throw new Exception("Subject does not exist"); }
+        }
+        public bool ContainsSubject(Subject subsub)
+        {
+            return this.ChosenSubjects.Contains(subsub);
+        }
     }
 }
